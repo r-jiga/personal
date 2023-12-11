1847014755376f259cac4eae7c850b512bdd73c9
r-jiga <142721006+r-jiga@users.noreply.github.com>
2023-12-11 14:06:57 +0200

    update loops chapter

--- loops/mario.py
+++ loops/mario.py
@@ -21,7 +21,36 @@
 def main():
     print_square(3)
 
+
 def print_square(size):
-    
+    for i in range(size):
+        print_row(size)
+
+
+# def print_row(width):
+#     print("#" * 3)
+
+# def print_square(size):
+#     for i in range(size):
+#         print("#" * size)
+
+# def print_square(size):
+
+#     # For each row in square
+#     for i in range(size):
+
+#         # For each brick in row
+#         for j in range(size):
+
+#             # Print brick
+#             print("#", end="")
+#         print()
+
+def print_square(size):
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
 
 main()
