8d7f30d8ebaabcb7819338ec8e0096c1498cfedb
r-jiga <142721006+r-jiga@users.noreply.github.com>
2023-12-11 12:28:57 +0200

    loops

---
+++ loops/mario.py
@@ -0,0 +1,27 @@
+# print("#")
+# print("#")
+# print("#")
+
+# for _ in range(3):
+#     print("#")
+
+# def main():
+#     print_column(3)
+
+# def print_column(height):
+#     for _ in range(height):
+#         print("#")
+
+# def main():
+#     print_row(4)
+
+# def print_row(width):
+#     print("?" * width)
+
+def main():
+    print_square(3)
+
+def print_square(size):
+    
+
+main()
